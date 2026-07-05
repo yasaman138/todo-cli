5aa0d193687d1d3193312526ded597a90972a425
Yasaman108 <2ujasnm@gmail.com>
2026-07-06 00:31:57 +0330

    feat: add the skeletons of all main commands

--- src/todo/cli.py
+++ src/todo/cli.py
@@ -1,3 +1,32 @@
 import typer
 
 app = typer.Typer(add_completion=False, help="A simple, fast command-line TODO manager.")
+
+
+@app.command
+def add() -> None:
+    """add a new task"""
+
+
+@app.command
+def list_tasks() -> None:
+    """list tasks"""
+
+
+@app.command
+def done() -> None:
+    """mark a task as done"""
+
+
+@app.command
+def remove() -> None:
+    """remove a task"""
+
+
+@app.command
+def clear() -> None:
+    """remove all tasks"""
+
+
+if __name__ == "__main__":
+    app()
